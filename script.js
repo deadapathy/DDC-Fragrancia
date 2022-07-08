ccd00f204fa5c30118be0bcc14facf1a619f41f6
deadapathy <maks_vaskov@bk.ru>
2022-07-08 14:53:21 +0600

    transfer and convert form final

--- script.js
+++ script.js
@@ -8,17 +8,21 @@ $(function rdBtn() {
 
     $("#consumption").on('click', function () {
         $("#block2").hide()
+        $("#block3").hide()
         $("#block1").show("slow");
+
     })
 
     $("#transfer").on('click', function () {
         $("#block1").hide();
+        $("#block3").hide()
         $("#block2").show("slow");
     })
 
     $("#convert").on('click', function () {
         $("#block1").hide();
         $("#block2").hide();
+        $("#block3").show("slow");
     })
 
 })
